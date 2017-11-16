bd70f84882b8906b010954f00c4f4a084c20c18e
NextHendrix <chris.jones.492@gmail.com>
2017-11-16 15:34:32 +0000

    flycheck

--- emacs/init.el
+++ emacs/init.el
@@ -1,5 +1,9 @@
-;;; NextHendrix init file - actually no evil this time edition
+;;; init --- Actually no evil-mode this time edition
 
+;;; Commentary:
+;; NextHendrix - Started 16/11/2017
+
+;;; Code:
 ;; Remove crusty UI
 (tool-bar-mode 0)
 (menu-bar-mode 0)
@@ -8,7 +12,7 @@
 ;; Startup
 (setq inhibit-startup-screen t) ; no splash
 (setq initial-major-mode 'org-mode) ; scratch be org
-(setq initial-scratch-message "") ; empty scratch 
+(setq initial-scratch-message "") ; empty scratch
 (setq vc-follow-symlinks t) ; don't prompt for following symlinks
 
 ;; Stop emacs shitting files all over the place
@@ -19,23 +23,26 @@
 
 ;; Package stuff
 (require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos)) ; add melpa
                     (not (gnutls-available-p))))
        (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
   (add-to-list 'package-archives (cons "melpa" url) t))
+
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize)
+
+(package-initialize) ; go!
 
 (unless (package-installed-p 'magit) ; only refresh if nothing's installed
   (package-refresh-contents))
 
-(defvar mypackages '(magit
+(defvar init--mypackages '(magit
 		     ivy
-		     counsel))
+		     counsel
+		     flycheck))
 
-(dolist (p mypackages) ; install my packages
+(dolist (p init--mypackages) ; install my packages
   (when (not (package-installed-p p))
     (package-install p)))
 
@@ -52,3 +59,9 @@
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
+
+;; Flycheck
+(global-flycheck-mode t)
+
+(provide 'init)
+;;; init.el ends here
