1b0ee75b0fb724ccbcaf8960302471ec9520cf89
NextHendrix <chris.jones.492@gmail.com>
2018-04-03 05:43:14 +0100

    xmonad Music workspace floating layout

--- XMonad/xmonad.hs
+++ XMonad/xmonad.hs
@@ -2,14 +2,14 @@ import           System.Exit
 import           System.IO
 import           XMonad
 import qualified XMonad.Actions.Navigation2D        as N
-import           XMonad.Config.Desktop
 import           XMonad.Hooks.DynamicLog
-import XMonad.Util.SpawnOnce
 import           XMonad.Hooks.ManageDocks
 import           XMonad.Hooks.ManageHelpers
 import           XMonad.Layout.BinarySpacePartition
 import           XMonad.Layout.Maximize
 import           XMonad.Layout.NoBorders            (smartBorders)
+import           XMonad.Layout.PerWorkspace
+import           XMonad.Layout.SimplestFloat
 import           XMonad.Prompt
 import           XMonad.Prompt.Shell
 import qualified XMonad.StackSet                    as W
@@ -29,89 +29,96 @@ main = do
       False $
     docks $
     def
-    { manageHook = manageDocks <+> (isFullscreen --> doFullFloat) <+> manageHook def
-    , layoutHook = smartBorders . avoidStruts $ myLayout
-    , startupHook = do
-        spawn "setxkbmap gb -option compose:ralt"
-        spawn "xmodmap .Xmodmap"
-        spawn "emacs --daemon &"
-        spawn "compton --config=/home/chris/.config/compton/compton.conf &"
-        spawn "urxvtd &"
-    , logHook =
-        dynamicLogWithPP
-          xmobarPP
-          { ppOutput = hPutStrLn xmproc
-          , ppTitle = xmobarColor "green" "" . shorten 70
-          }
-    , modMask = mod4Mask -- use windows key
-    , terminal = myTerm
-    , keys = myKeys
-    , workspaces = myWorkspaces
-    , normalBorderColor = "#000000"
-    , focusedBorderColor = "#FFFFFF"
-    }
+      { manageHook =
+          manageDocks <+> (isFullscreen --> doFullFloat) <+> manageHook def
+      , layoutHook =
+          onWorkspace
+            "Music"
+            (smartBorders . avoidStruts $ (maximize simplestFloat) ||| Full) $
+          smartBorders . avoidStruts $ myLayout
+      , startupHook =
+          do spawn "setxkbmap gb -option compose:ralt"
+             spawn "emacs --daemon &"
+             spawn "compton --config=/home/chris/.config/compton/compton.conf &"
+             spawn "urxvtd &"
+             spawn "xmodmap .Xmodmap"
+             spawn "xset r rate 175 175"
+             spawn "xrdb .Xresources"
+      , logHook =
+          dynamicLogWithPP
+            xmobarPP
+              { ppOutput = hPutStrLn xmproc
+              , ppTitle = xmobarColor "green" "" . shorten 70
+              }
+      , modMask = mod4Mask -- use windows key
+      , terminal = myTerm
+      , keys = myKeys
+      , workspaces = myWorkspaces
+      , normalBorderColor = "#000000"
+      , focusedBorderColor = "#FFFFFF"
+      }
 
 myLayout = maximize emptyBSP ||| Full
 
 launcherConfig :: XPConfig
 launcherConfig =
   def
-  { font =
-      "xft:Input Mono:size=10:antialias=true:hinting=full"
-  , height = 38
-  , position = Bottom
-  }
+    { font = "xft:Input Mono:size=10:antialias=true:hinting=full"
+    , height = 38
+    , position = Bottom
+    }
 
 myKeys conf =
-  mkKeymap conf
-  [ ("M-<Return>", spawn (terminal conf))
-  , ("M-<Space>", spawn editor)
-  , ("<Print>", spawn "scrot -q100")
-  , ("M-d", shellPrompt launcherConfig)
-  , ("M-b", spawn browser)
-  , ("M-S-b", spawn pBrowser)
-  , ("M-S-<Space>", sendMessage ToggleStruts)
-  , ("M-q", kill)
-  , ("M-S-r", spawn "xmonad --recompile && xmonad --restart")
-  , ("M-e", sendMessage NextLayout)
-  , ("M-t", withFocused $ windows . W.sink)
-  , ("M-`", windows $ W.greedyView "Control Centre")
-  , ("M-1", windows $ W.greedyView "Home")
-  , ("M-2", windows $ W.greedyView "Emacs")
-  , ("M-3", windows $ W.greedyView "Music")
-  , ("M-4", windows $ W.greedyView "Steam")
-  , ("M-5", windows $ W.greedyView "5")
-  , ("M-6", windows $ W.greedyView "6")
-  , ("M-7", windows $ W.greedyView "7")
-  , ("M-8", windows $ W.greedyView "8")
-  , ("M-9", windows $ W.greedyView "9")
-  , ("M-S-`", windows $ W.shift "Control Centre")
-  , ("M-S-1", windows $ W.shift "Home")
-  , ("M-S-2", windows $ W.shift "Emacs")
-  , ("M-S-3", windows $ W.shift "Music")
-  , ("M-S-4", windows $ W.shift "Steam")
-  , ("M-S-5", windows $ W.shift "5")
-  , ("M-S-6", windows $ W.shift "6")
-  , ("M-S-7", windows $ W.shift "7")
-  , ("M-S-8", windows $ W.shift "8")
-  , ("M-S-9", windows $ W.shift "9")
-  , ("M-r", sendMessage Rotate)
-  , ("M-a", sendMessage Balance)
-  , ("M-S-a", sendMessage Equalize)
-  , ("M-C-h", sendMessage $ ExpandTowards L)
-  , ("M-C-j", sendMessage $ ExpandTowards D)
-  , ("M-C-k", sendMessage $ ExpandTowards U)
-  , ("M-C-l", sendMessage $ ExpandTowards R)
-  , ("M-s", sendMessage $ Swap)
-  , ("M-m", withFocused (sendMessage . maximizeRestore))
-  , ("M-S-q", io (exitWith ExitSuccess))
-  , ("<Print>", spawn "spectacle")
-  , ("<XF86AudioRaiseVolume>", spawn "pamixer -u -i 5 --allow-boost")
-  , ("<XF86AudioLowerVolume>", spawn "pamixer -d 5 --allow-boost")
-  , ("<XF86AudioMute>", spawn "pamixer -t")
-  , ("<XF86MonBrightnessUp>", spawn "xbacklight -inc 10")
-  , ("<XF86MonBrightnessDown>", spawn "xbacklight -dec 10")
-  ]
+  mkKeymap
+    conf
+    [ ("M-<Return>", spawn (terminal conf))
+    , ("M-<Space>", spawn editor)
+    , ("<Print>", spawn "scrot -q100")
+    , ("M-d", shellPrompt launcherConfig)
+    , ("M-b", spawn browser)
+    , ("M-S-b", spawn pBrowser)
+    , ("M-S-<Space>", sendMessage ToggleStruts)
+    , ("M-q", kill)
+    , ("M-S-r", spawn "xmonad --recompile && xmonad --restart")
+    , ("M-e", sendMessage NextLayout)
+    , ("M-t", withFocused $ windows . W.sink)
+    , ("M-`", windows $ W.greedyView "Control Centre")
+    , ("M-1", windows $ W.greedyView "Home")
+    , ("M-2", windows $ W.greedyView "Emacs")
+    , ("M-3", windows $ W.greedyView "Music")
+    , ("M-4", windows $ W.greedyView "Steam")
+    , ("M-5", windows $ W.greedyView "5")
+    , ("M-6", windows $ W.greedyView "6")
+    , ("M-7", windows $ W.greedyView "7")
+    , ("M-8", windows $ W.greedyView "8")
+    , ("M-9", windows $ W.greedyView "9")
+    , ("M-S-`", windows $ W.shift "Control Centre")
+    , ("M-S-1", windows $ W.shift "Home")
+    , ("M-S-2", windows $ W.shift "Emacs")
+    , ("M-S-3", windows $ W.shift "Music")
+    , ("M-S-4", windows $ W.shift "Steam")
+    , ("M-S-5", windows $ W.shift "5")
+    , ("M-S-6", windows $ W.shift "6")
+    , ("M-S-7", windows $ W.shift "7")
+    , ("M-S-8", windows $ W.shift "8")
+    , ("M-S-9", windows $ W.shift "9")
+    , ("M-r", sendMessage Rotate)
+    , ("M-a", sendMessage Balance)
+    , ("M-S-a", sendMessage Equalize)
+    , ("M-C-h", sendMessage $ ExpandTowards L)
+    , ("M-C-j", sendMessage $ ExpandTowards D)
+    , ("M-C-k", sendMessage $ ExpandTowards U)
+    , ("M-C-l", sendMessage $ ExpandTowards R)
+    , ("M-s", sendMessage $ Swap)
+    , ("M-m", withFocused (sendMessage . maximizeRestore))
+    , ("M-S-q", io (exitWith ExitSuccess))
+    , ("<Print>", spawn "spectacle")
+    , ("<XF86AudioRaiseVolume>", spawn "pamixer -u -i 5 --allow-boost")
+    , ("<XF86AudioLowerVolume>", spawn "pamixer -d 5 --allow-boost")
+    , ("<XF86AudioMute>", spawn "pamixer -t")
+    , ("<XF86MonBrightnessUp>", spawn "xbacklight -inc 10")
+    , ("<XF86MonBrightnessDown>", spawn "xbacklight -dec 10")
+    ]
 
 editor :: String
 editor = "emacsclient -c"
