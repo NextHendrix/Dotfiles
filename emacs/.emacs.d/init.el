9f3322e1c97c58f13b54e51d04d0be25bfb324da
NextHendrix <chris.jones.492@gmail.com>
2018-11-29 03:45:44 +0000

    >lsp-ui

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -111,10 +111,6 @@
 
 ;; Haskell
 (require 'haskell-mode)
-(require 'lsp-ui)
-(require 'lsp-haskell)
-(add-hook 'lsp-mode-hook 'lsp-ui-mode)
-(add-hook 'haskell-mode-hook #'lsp-haskell-enable)
 (add-hook 'haskell-mode-hook 'flycheck-mode)
 
 
