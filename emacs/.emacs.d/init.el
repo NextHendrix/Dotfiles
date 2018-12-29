f057356baf7ff2c0cfb8757e2a64520c0c616123
NextHendrix <chris.jones.492@gmail.com>
2018-12-29 00:53:15 +0000

    new .emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,54 +1,22 @@
-;;; init.el --- NextHendrix init
-;;; Commentary:
-;;; Code:
-;; Set load path for extra stuff
-(add-to-list 'load-path "~/.emacs.d/lisp")
-(setq exec-path (append '("~/.local/bin/") exec-path))
+;;; NextHendrix init (innit)
 
-;; Custom file
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file 'noerror)
-
-;; Fuck off with your stupid~ #tmpfiles# bullshit
+;; Disregard tmpfiles
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
-;; Package memery
-(setq package-list
-      '(magit
-	nlinum
-	auctex
-	flycheck
-	guru-mode
-	ace-window
-	counsel
-	company
-	haskell-mode
-	nginx-mode
-	nov
-	pdf-tools
-	))
-
-(require 'package)
-(setq package-enable-at-startup t)
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ("melpa" . "https://melpa.org/packages/")))
-(package-initialize)
-
-(unless package-archive-contents
-  (package-refresh-contents))
-
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
+;; Custom file
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
 
 ;; Visual Tweaks
 (menu-bar-mode 0)
 (tool-bar-mode 0)
+(tooltip-mode 0)
 (scroll-bar-mode 0)
 (column-number-mode t)
+(line-number-mode t)
 (show-paren-mode t)
 (display-time-mode t)
 (global-visual-line-mode)
@@ -62,80 +30,44 @@
 ;; Follow Symlinks
 (setq vc-follow-symlinks t)
 
-;; Org Mode
-(require 'org)
-(setq org-format-latex-options
-      (plist-put org-format-latex-options :scale 2.5))
+;; Package Memery
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
 
-;; Guru Mode
-(require 'guru-mode)
-(guru-global-mode t)
-
-;; Ace Windows
-(require 'ace-window)
-(global-set-key (kbd "C-x o") 'ace-window)
-
-;; Counsel Mode
-(require 'counsel)
-(counsel-mode t)
-(global-set-key (kbd "C-s") 'swiper)
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
-(setq ivy-count-format "%d/%d ")
-
-;; IBuffer
-(global-set-key (kbd "C-x C-b") 'ibuffer)
-(autoload 'ibuffer "ibuffer" "List Buffers." t)
-
-;; Company
-(require 'company)
-(global-company-mode t)
+;; Use use-package package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
 
 ;; Flycheck
-(require 'flycheck)
-(global-flycheck-mode t)
-(setq flycheck-global-modes t)
+(use-package flycheck
+  :ensure t
+  :commands global-flycheck-mode
+  :init
+  (global-flycheck-mode))
 
-;; PDF Tools
-(require 'pdf-tools)
-(pdf-tools-install)
-(setq pdf-view-use-imagemagick t)
-(setq pdf-view-use-dedicated-register t)
-(setq pdf-view-use-scaling t)
-(add-hook 'pdf-view-mode-hook 'auto-revert-mode)
-
-;; Prog Mode
-(add-hook 'prog-mode-hook 'nlinum-mode)
-;(add-hook 'prog-mode-hook 'prettify-symbols-mode)
-
-;; Python
-(require 'python)
-(setq python-shell-interpreter "python3")
-
-;; Haskell
-(require 'haskell-mode)
-(add-hook 'haskell-mode-hook 'flycheck-mode)
+;; Guru Mode
+(use-package guru-mode
+  :ensure t
+  :commands guru-global-mode
+  :init
+  (guru-global-mode))
 
 ;; RCIRC
-(require 'rcirc)
-(setq rcirc-server-alist
-      '(("finickitively.co.uk"
-	 :port 6697
-	 :encryption tls
-	 :nick "nh"
-	 :user-name "NextHendrix"
-	 :full-name "NextHendrix")))
+(use-package rcirc
+  :ensure t
+  :commands irc
+  :config
+  (setq rcirc-server-alist
+	'(("finickitively.co.uk"
+	   :port 6697
+	   :encryption tls))))
 
-
-;; ERC
-;(require 'erc)
-;(require 'tls)
-;(require 'erc-settings)
-;; (require 'erc-tex)
-;; (add-hook 'erc-mode-hook 'erc-tex-enable)
-;; (setq erc-tex-image-size 2.5)
-
+;; Magit
+(use-package magit
+  :ensure t)
 ;; Done!
 (message "Nice one mate")
-(provide 'init)
-;;; init.el ends here
